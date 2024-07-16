972a47becfa3cc0effad3ec73298690a0111394c
Faizaiddiqui <149603867+Faizaiddiqui@users.noreply.github.com>
2024-07-16 19:02:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7218519553:AAETsHcASW2o_CKamHvt-Z9eJyanIbXN7Jc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@FaizalinaQureshi")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6272704721")
 
 
 
